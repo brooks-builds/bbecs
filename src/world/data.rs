1acdc57bcd223e00cbc64ab8e3125c6cdbf70c12
Brooks Patton <brooks@brookzerker.com>
2021-02-08 20:30:43 -0700

    wip - switching to dynamic dispatch for storing the data

--- src/world/data.rs
+++ src/world/data.rs
@@ -1,18 +1,45 @@
-#[derive(Debug, PartialEq)]
-pub struct Data {}
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+};
+
+#[derive(Debug)]
+pub struct Data {
+    pub data: HashMap<TypeId, Vec<Box<dyn Any>>>,
+}
 
 impl Data {
     pub fn new() -> Self {
-        Self {}
+        let data = HashMap::new();
+        Self { data }
+    }
+
+    pub fn insert(&mut self, new_data: impl Any) {
+        self.data
+            .insert(new_data.type_id(), vec![Box::new(new_data)]);
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::any::Any;
+
     use super::*;
 
     #[test]
     fn create_new_data() {
         let data = Data::new();
+        assert_eq!(data.data.len(), 0);
+    }
+
+    #[test]
+    fn insert_data() {
+        let mut data = Data::new();
+        let test_data_id = 32.type_id();
+        data.insert(32);
+        let raw_data = data.data.get(&test_data_id).unwrap()[0]
+            .downcast::<i32>()
+            .unwrap();
+        assert_eq!(raw_data, Box::new(32));
     }
 }
--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,3 +1,5 @@
+use std::{any::Any, fmt::Debug};
+
 use data::Data;
 
 mod data;
@@ -11,16 +13,41 @@ impl World {
         let data = Data::new();
         Self { data }
     }
+
+    pub fn insert_entity(&mut self, entity_data: Vec<impl Any>) {
+        for entity_part in entity_data {
+            self.data.insert(entity_part);
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::any::Any;
+
     use super::{data::Data, World};
 
     #[test]
     fn create_new_world() {
         let world = World::new();
-        let expected_data = Data::new();
-        assert_eq!(world.data, expected_data);
+        assert_eq!(world.data.data.len(), 0);
+    }
+
+    #[test]
+    fn insert_entity_into_world() {
+        let mut world = World::new();
+        let location = TestLocation { x: 0.0, y: 0.0 };
+        world.insert_entity(vec![location]);
+        let data = world.data.data.get(&location.type_id()).unwrap()[0]
+            .downcast::<TestLocation>()
+            .unwrap()
+            .clone();
+        assert_eq!(*data, location);
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq)]
+    struct TestLocation {
+        x: f32,
+        y: f32,
     }
 }
