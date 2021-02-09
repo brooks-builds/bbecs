e57dcc54a8ee103a174b51a2b73c11aa3c33140b
Brooks Patton <brooks@brookzerker.com>
2021-02-09 07:35:17 -0700

    Using an RC now instead of a Box

--- src/world/data.rs
+++ src/world/data.rs
@@ -1,11 +1,12 @@
 use std::{
     any::{Any, TypeId},
     collections::HashMap,
+    rc::Rc,
 };
 
 #[derive(Debug)]
 pub struct Data {
-    pub data: HashMap<TypeId, Vec<Box<dyn Any>>>,
+    pub data: HashMap<TypeId, Vec<Rc<dyn Any>>>,
 }
 
 impl Data {
@@ -16,7 +17,7 @@ impl Data {
 
     pub fn insert(&mut self, new_data: impl Any) {
         self.data
-            .insert(new_data.type_id(), vec![Box::new(new_data)]);
+            .insert(new_data.type_id(), vec![Rc::new(new_data)]);
     }
 }
 
@@ -38,8 +39,9 @@ mod tests {
         let test_data_id = 32.type_id();
         data.insert(32);
         let raw_data = data.data.get(&test_data_id).unwrap()[0]
+            .clone()
             .downcast::<i32>()
             .unwrap();
-        assert_eq!(raw_data, Box::new(32));
+        assert_eq!(raw_data, Rc::new(32));
     }
 }
--- src/world/mod.rs
+++ src/world/mod.rs
@@ -39,9 +39,9 @@ mod tests {
         let location = TestLocation { x: 0.0, y: 0.0 };
         world.insert_entity(vec![location]);
         let data = world.data.data.get(&location.type_id()).unwrap()[0]
+            .clone()
             .downcast::<TestLocation>()
-            .unwrap()
-            .clone();
+            .unwrap();
         assert_eq!(*data, location);
     }
 
