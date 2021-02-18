cccb115b358e0e6bdf8f9a064ef7804eefe0d41b
Brooks Patton <brooks@brookzerker.com>
2021-02-18 08:30:26 -0700

    Can save a point as a resource

--- src/resources/resource.rs
+++ src/resources/resource.rs
@@ -1,26 +1,39 @@
 use ggez::graphics::{Color, Mesh};
 
+use crate::components::point::Point;
+
 pub enum Resource {
     Color(Color),
     Mesh(Mesh),
+    Point(Point),
 }
 
 impl Resource {
-    /// Cast to type Color. If the cast fails then we are going to panic
     pub fn cast_color(&self) -> &Color {
         if let Self::Color(color) = self {
             color
         } else {
-            panic!("Cannot cast to type color as it is another type");
+            panic!(self.create_error_message("color"));
         }
     }
 
-    /// Cast to type Mesh, if the cast fails then we are going to panic.
     pub fn cast_mesh(&self) -> &Mesh {
         if let Self::Mesh(mesh) = self {
             mesh
         } else {
-            panic!("Cannot cast to type mesh as it is another type");
+            panic!(self.create_error_message("mesh"));
         }
+    }
+
+    pub fn cast_point(&self) -> &Point {
+        if let Self::Point(point) = self {
+            point
+        } else {
+            panic!(self.create_error_message("point"));
+        }
+    }
+
+    fn create_error_message(&self, type_name: &str) -> String {
+        format!("Cannot cast to type {} as it is another type", type_name)
     }
 }
